8fd14767294d21e9bf93a96dc77fd3dd4588881c
KxFxN <sakarin14184@gmail.com>
2024-07-19 15:41:09 +0700

    fix layout

--- src/app/components/CardReview.js
+++ src/app/components/CardReview.js
@@ -35,7 +35,7 @@ function CardReview() {
     },
   ];
   return (
-    <div className="grid grid-cols-1 gap-5 sm:grid-cols-3 md:grid-cols-2 xl:grid-cols-3">
+    <div className="grid grid-cols-1 gap-5 sm:grid-cols-3 md:grid-cols-2 xl:grid-cols-2">
       {imageReview.map((image, index) => (
         <div
           key={index}
